d0379f50718c6c2f216f07423ef0eeed782f9ea8
Ted Spence <ted@spence.net>
2023-07-09 10:24:37 -0700

    Generate interfaces for DotNet client

--- tools/SdkGenerator/SdkGenerator/Languages/CSharpSdk.cs
+++ tools/SdkGenerator/SdkGenerator/Languages/CSharpSdk.cs
@@ -236,8 +236,14 @@ public static class CSharpSdk
     private static async Task ExportEndpoints(GeneratorContext context)
     {
         var clientsDir = Path.Combine(context.Project.Csharp.Folder, "src", "Clients");
+        var interfacesDir = Path.Combine(context.Project.Csharp.Folder, "src", "Interfaces");
         Directory.CreateDirectory(clientsDir);
+        Directory.CreateDirectory(interfacesDir);
         foreach (var clientFile in Directory.EnumerateFiles(clientsDir, "*.cs"))
+        {
+            File.Delete(clientFile);
+        }
+        foreach (var clientFile in Directory.EnumerateFiles(interfacesDir, "*.cs"))
         {
             File.Delete(clientFile);
         }
@@ -245,110 +251,169 @@ public static class CSharpSdk
         // Gather a list of unique categories
         foreach (var cat in context.Api.Categories)
         {
-            var sb = new StringBuilder();
-
-            // Construct header
-            sb.AppendLine(FileHeader(context.Project));
-            sb.AppendLine("using System;");
-            sb.AppendLine("using System.Collections.Generic;");
-            sb.AppendLine("using System.Net.Http;");
-            sb.AppendLine("using System.Threading.Tasks;");
-            sb.AppendLine($"using {context.Project.Csharp.Namespace}.Models;");
-            sb.AppendLine();
-            sb.AppendLine();
-            sb.AppendLine($"namespace {context.Project.Csharp.Namespace}.Clients");
-            sb.AppendLine("{");
-            sb.AppendLine("    /// <summary>");
-            sb.AppendLine($"    /// API methods related to {cat}");
-            sb.AppendLine("    /// </summary>");
-            sb.AppendLine($"    public class {cat}Client");
-            sb.AppendLine("    {");
-            sb.AppendLine($"        private readonly {context.Project.Csharp.ClassName} _client;");
-            sb.AppendLine();
-            sb.AppendLine("        /// <summary>");
-            sb.AppendLine("        /// Constructor");
-            sb.AppendLine("        /// </summary>");
-            sb.AppendLine($"        public {cat}Client({context.Project.Csharp.ClassName} client)");
-            sb.AppendLine("        {");
-            sb.AppendLine("            _client = client;");
-            sb.AppendLine("        }");
-
-            // Run through all APIs
-            foreach (var endpoint in context.Api.Endpoints)
-            {
-                if (endpoint.Category == cat && !endpoint.Deprecated)
-                {
-                    sb.AppendLine();
-                    sb.Append(MarkdownToDocblock(endpoint.DescriptionMarkdown, 8, endpoint.Parameters));
-
-                    // Figure out the parameter list
-                    var paramList = new List<string>();
-                    foreach (var p in from p in endpoint.Parameters orderby p.Required descending select p)
-                    {
-                        var isNullable = !p.Required || p.Nullable;
-                        var typeName = FixupType(context, p.DataType, p.IsArray, isNullable);
-                        var paramText = $"{typeName} {p.Name.ToVariableName()}{(isNullable ? " = null" : "")}";
-                        paramList.Add(paramText);
-                    }
-
-                    // Do we need to specify options?
-                    var options = (from p in endpoint.Parameters where p.Location == "query" select p).ToList();
-                    var isFileUpload = (from p in endpoint.Parameters where p.Location == "form" select p).Any();
-
-                    // What is our return type?  Note that we're assuming this can be non-nullable since the
-                    // response class already handles the case where the value ends up being null.
-                    var returnType = FixupType(context, endpoint.ReturnDataType.DataType, endpoint.ReturnDataType.IsArray,
-                        false);
-
-                    // Write the method
-                    var modernSignature =
-                        $"        public async Task<{context.Project.Csharp.ResponseClass}<{returnType}>> {endpoint.Name.ToProperCase()}({string.Join(", ", paramList)})";
-                    sb.AppendLine(modernSignature);
-
-                    sb.AppendLine("        {");
-                    sb.AppendLine($"            var url = $\"{endpoint.Path}\";");
-
-                    // Add query string parameter options
-                    if (options.Count > 0)
-                    {
-                        sb.AppendLine("            var options = new Dictionary<string, object>();");
-                        foreach (var o in options)
-                        {
-                            sb.AppendLine(
-                                !o.Required
-                                    ? $"            if ({o.Name.ToVariableName()} != null) {{ options[\"{o.Name}\"] = {o.Name.ToVariableName()}; }}"
-                                    : $"            options[\"{o.Name}\"] = {o.Name.ToVariableName()};");
-                        }
-                    }
-
-                    // Determine the HTTP method
-                    var method = $"HttpMethod.{endpoint.Method.ToProperCase()}";
-                    if (string.Equals(endpoint.Method, "PATCH", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        method = "new HttpMethod(\"PATCH\")";
-                    }
-
-                    // Send the request
-                    var hasBody = (from p in endpoint.Parameters where p.Location == "body" select p).Any();
-                    var optionsStr = options.Count > 0 ? ", options" : ", null";
-                    var bodyStr = hasBody ? ", body" : ", null";
-                    var fileStr = isFileUpload ? ", filename" : ", null";
-                    sb.AppendLine(
-                        $"            return await _client.Request<{returnType}>({method}, url{optionsStr}{bodyStr}{fileStr});");
-                    sb.AppendLine("        }");
-                }
-            }
-
-            // Close out the class and namespace
-            sb.AppendLine("    }");
-            sb.AppendLine("}");
-
-            // Write this category to a file
-            var modulePath = Path.Combine(clientsDir, $"{cat}Client.cs");
-            await File.WriteAllTextAsync(modulePath, sb.ToString());
+            await ExportCategoryClient(context, cat, clientsDir);
+            await ExportCategoryInterface(context, cat, interfacesDir);
         }
     }
 
+    private static async Task ExportCategoryClient(GeneratorContext context, string cat, string clientsDir)
+    {
+        var sb = new StringBuilder();
+
+        // Construct header
+        sb.AppendLine(FileHeader(context.Project));
+        sb.AppendLine("using System;");
+        sb.AppendLine("using System.Collections.Generic;");
+        sb.AppendLine("using System.Net.Http;");
+        sb.AppendLine("using System.Threading.Tasks;");
+        sb.AppendLine($"using {context.Project.Csharp.Namespace}.Interfaces;");
+        sb.AppendLine($"using {context.Project.Csharp.Namespace}.Models;");
+        sb.AppendLine();
+        sb.AppendLine();
+        sb.AppendLine($"namespace {context.Project.Csharp.Namespace}.Clients");
+        sb.AppendLine("{");
+        sb.AppendLine("    /// <summary>");
+        sb.AppendLine($"    /// API methods related to {cat}");
+        sb.AppendLine("    /// </summary>");
+        sb.AppendLine($"    public class {cat}Client : I{cat}Client");
+        sb.AppendLine("    {");
+        sb.AppendLine($"        private readonly {context.Project.Csharp.ClassName} _client;");
+        sb.AppendLine();
+        sb.AppendLine("        /// <summary>");
+        sb.AppendLine("        /// Constructor");
+        sb.AppendLine("        /// </summary>");
+        sb.AppendLine($"        public {cat}Client({context.Project.Csharp.ClassName} client)");
+        sb.AppendLine("        {");
+        sb.AppendLine("            _client = client;");
+        sb.AppendLine("        }");
+
+        // Run through all APIs
+        foreach (var endpoint in context.Api.Endpoints.Where(endpoint => endpoint.Category == cat && !endpoint.Deprecated))
+        {
+            sb.AppendLine();
+            sb.Append(MarkdownToDocblock(endpoint.DescriptionMarkdown, 8, endpoint.Parameters));
+
+            // Figure out the parameter list
+            var paramList = new List<string>();
+            foreach (var p in from p in endpoint.Parameters orderby p.Required descending select p)
+            {
+                var isNullable = !p.Required || p.Nullable;
+                var typeName = FixupType(context, p.DataType, p.IsArray, isNullable);
+                var paramText = $"{typeName} {p.Name.ToVariableName()}{(isNullable ? " = null" : "")}";
+                paramList.Add(paramText);
+            }
+
+            // Do we need to specify options?
+            var options = (from p in endpoint.Parameters where p.Location == "query" select p).ToList();
+            var isFileUpload = (from p in endpoint.Parameters where p.Location == "form" select p).Any();
+
+            // What is our return type?  Note that we're assuming this can be non-nullable since the
+            // response class already handles the case where the value ends up being null.
+            var returnType = FixupType(context, endpoint.ReturnDataType.DataType, endpoint.ReturnDataType.IsArray,
+                false);
+
+            // Write the method
+            var modernSignature =
+                $"        public async Task<{context.Project.Csharp.ResponseClass}<{returnType}>> {endpoint.Name.ToProperCase()}({string.Join(", ", paramList)})";
+            sb.AppendLine(modernSignature);
+
+            sb.AppendLine("        {");
+            sb.AppendLine($"            var url = $\"{endpoint.Path}\";");
+
+            // Add query string parameter options
+            if (options.Count > 0)
+            {
+                sb.AppendLine("            var options = new Dictionary<string, object>();");
+                foreach (var o in options)
+                {
+                    sb.AppendLine(
+                        !o.Required
+                            ? $"            if ({o.Name.ToVariableName()} != null) {{ options[\"{o.Name}\"] = {o.Name.ToVariableName()}; }}"
+                            : $"            options[\"{o.Name}\"] = {o.Name.ToVariableName()};");
+                }
+            }
+
+            // Determine the HTTP method
+            var method = $"HttpMethod.{endpoint.Method.ToProperCase()}";
+            if (string.Equals(endpoint.Method, "PATCH", StringComparison.InvariantCultureIgnoreCase))
+            {
+                method = "new HttpMethod(\"PATCH\")";
+            }
+
+            // Send the request
+            var hasBody = (from p in endpoint.Parameters where p.Location == "body" select p).Any();
+            var optionsStr = options.Count > 0 ? ", options" : ", null";
+            var bodyStr = hasBody ? ", body" : ", null";
+            var fileStr = isFileUpload ? ", filename" : ", null";
+            sb.AppendLine(
+                $"            return await _client.Request<{returnType}>({method}, url{optionsStr}{bodyStr}{fileStr});");
+            sb.AppendLine("        }");
+        }
+
+        // Close out the class and namespace
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        // Write this category to a file
+        var modulePath = Path.Combine(clientsDir, $"{cat}Client.cs");
+        await File.WriteAllTextAsync(modulePath, sb.ToString());
+    }
+
+
+    private static async Task ExportCategoryInterface(GeneratorContext context, string cat, string interfacesDir)
+    {
+        var sb = new StringBuilder();
+
+        // Construct header
+        sb.AppendLine(FileHeader(context.Project));
+        sb.AppendLine("using System;");
+        sb.AppendLine("using System.Collections.Generic;");
+        sb.AppendLine("using System.Threading.Tasks;");
+        sb.AppendLine($"using {context.Project.Csharp.Namespace}.Models;");
+        sb.AppendLine();
+        sb.AppendLine();
+        sb.AppendLine($"namespace {context.Project.Csharp.Namespace}.Interfaces");
+        sb.AppendLine("{");
+        sb.AppendLine("    /// <summary>");
+        sb.AppendLine($"    /// API methods related to {cat}");
+        sb.AppendLine("    /// </summary>");
+        sb.AppendLine($"    public interface I{cat}Client");
+        sb.AppendLine("    {");
+
+        // Run through all APIs
+        foreach (var endpoint in context.Api.Endpoints.Where(endpoint => endpoint.Category == cat && !endpoint.Deprecated))
+        {
+            sb.AppendLine();
+            sb.Append(MarkdownToDocblock(endpoint.DescriptionMarkdown, 8, endpoint.Parameters));
+
+            // Figure out the parameter list
+            var paramList = new List<string>();
+            foreach (var p in from p in endpoint.Parameters orderby p.Required descending select p)
+            {
+                var isNullable = !p.Required || p.Nullable;
+                var typeName = FixupType(context, p.DataType, p.IsArray, isNullable);
+                var paramText = $"{typeName} {p.Name.ToVariableName()}{(isNullable ? " = null" : "")}";
+                paramList.Add(paramText);
+            }
+
+            // What is our return type?  Note that we're assuming this can be non-nullable since the
+            // response class already handles the case where the value ends up being null.
+            var returnType = FixupType(context, endpoint.ReturnDataType.DataType, endpoint.ReturnDataType.IsArray,
+                false);
+
+            // Write the method
+            sb.AppendLine($"        Task<{context.Project.Csharp.ResponseClass}<{returnType}>> {endpoint.Name.ToProperCase()}({string.Join(", ", paramList)});");
+        }
+
+        // Close out the interface and namespace
+        sb.AppendLine("    }");
+        sb.AppendLine("}");
+
+        // Write this category to a file
+        var modulePath = Path.Combine(interfacesDir, $"{cat}Client.cs");
+        await File.WriteAllTextAsync(modulePath, sb.ToString());
+    }
+    
     public static async Task Export(GeneratorContext context)
     {
         if (context.Project.Csharp == null)
@@ -363,6 +428,9 @@ public static class CSharpSdk
         await ScribanFunctions.ExecuteTemplate(context, 
             Path.Combine(".", "templates", "csharp", "ApiClient.cs.scriban"),
             Path.Combine(context.Project.Csharp.Folder, "src", context.Project.Csharp.ClassName + ".cs"));
+        await ScribanFunctions.ExecuteTemplate(context, 
+            Path.Combine(".", "templates", "csharp", "ApiInterface.cs.scriban"),
+            Path.Combine(context.Project.Csharp.Folder, "src", "Interfaces", context.Project.Csharp.ClassName + ".cs"));
         await ScribanFunctions.ExecuteTemplate(context, 
             Path.Combine(".", "templates", "csharp", "sdk.nuspec.scriban"),
             Path.Combine(context.Project.Csharp.Folder, context.Project.Csharp.ClassName + ".nuspec"));
