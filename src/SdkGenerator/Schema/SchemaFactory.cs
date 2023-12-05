72a8a33c5248d40252c17d6dc23ff84ffaf1c0dc
Ted Spence <ted@spence.net>
2023-12-05 10:44:54 -0800

    Address issues with readme file uploads
    Looks like some of the APIs have issues when they are file downloads.

--- src/SdkGenerator/Schema/SchemaFactory.cs
+++ src/SdkGenerator/Schema/SchemaFactory.cs
@@ -336,12 +336,19 @@ public static class SchemaFactory
                 {
                     if (response.Name.StartsWith("2"))
                     {
-                        response.Value.TryGetProperty("content", out var contentProp);
-                        contentProp.TryGetProperty("application/json", out var appJsonProp);
-                        foreach (var responseSchemaProp in appJsonProp.EnumerateObject())
+                        try
                         {
-                            item.ReturnDataType = GetTypeRef(context, responseSchemaProp);
-                            break;
+                            response.Value.TryGetProperty("content", out var contentProp);
+                            contentProp.TryGetProperty("application/json", out var appJsonProp);
+                            foreach (var responseSchemaProp in appJsonProp.EnumerateObject())
+                            {
+                                item.ReturnDataType = GetTypeRef(context, responseSchemaProp);
+                                break;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            context.LogError($"Failed to process endpoint {item?.Path ?? "Unknown Path"}: {ex.Message}");
                         }
                     }
                 }
