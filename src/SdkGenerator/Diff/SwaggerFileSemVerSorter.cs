6c5bcad1f30b6c3f8959ca2f60f3a7ada2cc2fa1
Ted Spence <ted@spence.net>
2024-08-13 13:49:02 -0700

    Tools for handling patch notes (#26)
    Make it possible to calculate patch notes and release name quickly.

---
+++ src/SdkGenerator/Diff/SwaggerFileSemVerSorter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Semver;
+
+namespace SdkGenerator.Diff;
+
+public class SwaggerFileSemVerSorter : IComparer<string>
+{
+    int IComparer<string>.Compare(string x, string y)
+    {
+        var semverX = GetSemVerFor(x);
+        var semverY = GetSemVerFor(y);
+        
+        // We want descending order, so Y compared to X
+        return semverY.CompareSortOrderTo(semverX);
+    }
+
+    private SemVersion GetSemVerFor(string filename)
+    {
+        var fileName = Path.GetFileNameWithoutExtension(filename);
+        var dashPos = fileName.IndexOf('-');
+        if (dashPos > 0)
+        {
+            var versionString = fileName[(dashPos + 1)..];
+            if (SemVersion.TryParse(versionString, SemVersionStyles.Any, out var semver))
+            {
+                return semver;
+            }
+        }
+
+        // Treat unnamed files as lowest
+        return new SemVersion(0, 0, 0);
+    }
+}
