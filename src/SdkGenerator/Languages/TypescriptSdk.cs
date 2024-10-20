ce03f537270f786a29b4407438673c7c5cb6f30c
Ted Spence <ted@spence.net>
2024-10-20 10:54:19 -0700

    Fixes for python and typescript

--- src/SdkGenerator/Languages/TypescriptSdk.cs
+++ src/SdkGenerator/Languages/TypescriptSdk.cs
@@ -367,7 +367,7 @@ public class TypescriptSdk : ILanguageSdk
         // Let's try using Scriban to populate these files
         await ScribanFunctions.ExecuteTemplateIfNotExists(context, 
             "SdkGenerator.Templates.ts.publish.yml.scriban",
-            context.MakePath(context.Project.Csharp.Folder, ".github", "workflows", "publish.yml"));
+            context.MakePath(context.Project.Typescript.Folder, ".github", "workflows", "publish.yml"));
         await ScribanFunctions.ExecuteTemplate(context, 
             "SdkGenerator.Templates.ts.ApiClient.scriban",
             context.MakePath(context.Project.Typescript.Folder, "src", context.Project.Typescript.ClassName + ".ts"));
